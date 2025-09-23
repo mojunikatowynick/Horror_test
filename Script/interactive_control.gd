fda89fee4a30f400331bf9f8ccc869170858ffa3
mojunikatowynick <148270188+mojunikatowynick@users.noreply.github.com>
2025-09-23 22:33:24 +0200

    zoom in works

--- Script/interactive_control.gd
+++ Script/interactive_control.gd
@@ -3,6 +3,8 @@ extends Node
 var mesh_owner: MeshInstance3D
 var _rotate_show_tem: bool = false
 @export var mouse_sensitivity: float = 0.09
+@export var zoom_speed: float = 0.2
+var mesh_zoom: float = 0.0
 
 func _ready() -> void:
 	mesh_owner = get_parent()
@@ -11,8 +13,17 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	_control_show_item()
 
+	if Input.is_action_just_pressed("ZoomIn"):
+		mesh_zoom = mesh_zoom + zoom_speed
+	if Input.is_action_just_pressed("ZoomOut"):
+		mesh_zoom -= zoom_speed
+
+	mesh_zoom = clampf(mesh_zoom, 0.0, 2.2)
+	mesh_owner.position.z = mesh_zoom
+
+
 func _control_show_item() -> void:
-	if Input.is_action_pressed("LMB"):
+	if Input.is_action_pressed("RMB"):
 		Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
 		_rotate_show_tem = true
 	else :
@@ -23,4 +34,4 @@ func _input(event:InputEvent) -> void:
 	if _rotate_show_tem:
 		var _mouse_input: = event is InputEventMouseMotion
 		if _mouse_input:
-			mesh_owner.rotate(Vector3(event.relative.y, event.relative.x, 0.0).normalized(),  mouse_sensitivity)
+			mesh_owner.rotate(Vector3(event.relative.y, event.relative.x, 1.0).normalized(),  mouse_sensitivity)
--- UI/ui.gd
+++ UI/ui.gd
@@ -4,14 +4,14 @@ var parent: Node3D
 @export var pause_menu: Control
 @export var show_menu: Control
 @export var show_item_mesh_instat_place: Node3D
-
+@onready var interactive_item_control : = preload("res://Script/interactive_control.tscn")
 var _is_paused: bool = false
 
 func _ready() -> void:
 	parent = get_parent()
 	pause_menu.visible = false
 	show_menu.visible = false
-	MessageBus.connect("interaction_item_show", _show_item)
+	MessageBus.connect("interaction_item_show", _show_interaced_item)
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("Pause"):
@@ -36,9 +36,11 @@ func resume() -> void:
 	for child in show_item_mesh_instat_place.get_children():
 		child.queue_free()
 
-func _show_item(_show_mesh: String) -> void:
+func _show_interaced_item(_show_mesh: String) -> void:
 	pause()
 	var temp_mesh := load(_show_mesh)
-	var instance :MeshInstance3D = temp_mesh.instantiate()
-	show_item_mesh_instat_place.add_child(instance)
+	var mesh_instance :MeshInstance3D = temp_mesh.instantiate()
+	show_item_mesh_instat_place.add_child(mesh_instance)
 	show_menu.visible = true
+	var controller_instance : = interactive_item_control.instantiate()
+	mesh_instance.add_child(controller_instance)
